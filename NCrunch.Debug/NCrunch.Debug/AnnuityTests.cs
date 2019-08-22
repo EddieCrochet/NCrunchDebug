97d8230c8c561f11d6bcca8455e067c259c2f132
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-08-21 19:10:02 -0500

    Wrote a unit test to check if method is calculating the FVA correctly for the application

--- NCrunch.Debug/NCrunch.Debug/AnnuityTests.cs
+++ NCrunch.Debug/NCrunch.Debug/AnnuityTests.cs
@@ -5,5 +5,12 @@ namespace NCrunch.Debug
 {
     public class AnnuityTests
     {
+        [Theory] // a theory in xUnit is a [Test] that takes parameters
+        [InlineData(1, 100, 5, 105)]
+        public void ComputesExpectedFutureValue(int n, decimal CF, decimal i, decimal FVA)
+        {
+            var actual = Annuity.ComputeFutureValue(n, CF, i);
+            Assert.Equal(FVA, actual);
+        }
     }
 }
